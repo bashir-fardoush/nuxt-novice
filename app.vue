e660a1543fa68b66632d1cbe467903cf77ed5283
Bashir Fardoush <bashirfardoush@gmail.com>
2023-03-09 19:49:59 +0600

    simple increment and decrement works fine

--- app.vue
+++ app.vue
@@ -2,5 +2,19 @@
   <div>
     <!-- <NuxtWelcome /> -->
     Hello NUXTJS, It's amazing to start it.
+
+    <hr>
+    <button @click="increment">Increment</button>
+    <p>{{ count }}</p>
+    <button @click="decrement">Decrement</button>
   </div>
 </template>
+<script setup>
+const count = ref(0);
+function increment(){
+  count.value++;
+}
+function decrement(){
+  count.value--;
+}
+</script>
